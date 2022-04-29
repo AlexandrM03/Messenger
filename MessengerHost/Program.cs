06cef18550c3d2f3fd200675c508720f07b814d5
Alexander <alexandrmozolevskiy@gmail.com>
2022-04-29 12:09:41 +0300

    Add project files.

---
+++ MessengerHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace MessengerHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(WCFService.MessengerService)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
