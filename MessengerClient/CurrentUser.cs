9693d14adeb9d7a7c6ec864c2ad37d93d40cba6c
Alexander <alexandrmozolevskiy@gmail.com>
2022-04-30 20:12:14 +0300

    Login view added + some logic

---
+++ MessengerClient/CurrentUser.cs
@@ -0,0 +1,23 @@
+﻿using MessengerClient.Logic.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerClient
+{
+    public class CurrentUser
+    {
+        public readonly static UserModel User = new UserModel();
+
+        public static void SetNewUser(UserModel user)
+        {
+            User.Id = user.Id;
+            User.Name = user.Name;
+            User.Surname = user.Surname;
+            User.Role = user.Role;
+            User.Avatar = user.Avatar;
+        }
+    }
+}
--- MessengerClient/Logic/ViewModel/LoginVM/LoginVM.cs
+++ MessengerClient/Logic/ViewModel/LoginVM/LoginVM.cs
@@ -1,19 +1,59 @@
-﻿using System;
+﻿using MessengerClient.Logic.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MessengerClient.Logic.ViewModel.LoginVM
 {
     public class LoginVM : BaseVM
     {
         private Window signWindow;
+        private LoginModel loginModel;
 
-        public LoginVM(Window signWindow)
+        public LoginModel LoginModel
         {
-            this.signWindow = signWindow;
+            get => loginModel; 
+            set
+            {
+                loginModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SignInCommand { get; private set; } 
+
+        public LoginVM(Window window)
+        {
+            signWindow = window;
+            LoginModel = new LoginModel();
+            SignInCommand = new DelegateCommand(SignIn);
+        }
+
+        private void SignIn(object obj)
+        {
+            if (!(obj is PasswordBox passwordBox))
+                return;
+            LoginModel.Password = passwordBox.Password;
+
+            if (LoginModel.Login == null || LoginModel.Password == null)
+            {
+                MessageBox.Show("Введите логин и пароль");
+                return;
+            }
+
+            Dictionary<string, string> userData;
+            
+            // TODO
+            if (CurrentUser.User.Role.Equals("user"))
+            {
+                signWindow.Close();
+                return;
+            }
         }
     }
 }
