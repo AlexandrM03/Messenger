831888e79e1bfb6b546a81c180371e143872d819
Alexander <alexandrmozolevskiy@gmail.com>
2022-05-05 19:19:35 +0300

    Admin start

---
+++ MessengerClient/Logic/ViewModel/AdminVM/AdminNavigation.cs
@@ -0,0 +1,29 @@
+﻿using MessengerClient.Presentation.View.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MessengerClient.Logic.ViewModel.AdminVM
+{
+    public class AdminNavigation
+    {
+        Dictionary<string, Page> pages = new Dictionary<string, Page>();
+
+        private readonly Page online;
+
+        public AdminNavigation()
+        {
+            online = new OnlineUsersPage();
+
+            pages.Add("online", online);
+        }
+
+        public Page GetPage(string name)
+        {
+            return pages[name];
+        }
+    }
+}
---
+++ MessengerClient/Logic/ViewModel/AdminVM/AdminVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MessengerClient.Logic.ViewModel.AdminVM
+{
+    public class AdminVM : BaseVM
+    {
+        private Page mainContent;
+        private AdminNavigation navigation;
+
+        public Page MainContent
+        {
+            get => mainContent;
+            set
+            {
+                mainContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand GoToOnlineUsersCommand { get; set; }
+        
+        public AdminVM()
+        {
+            navigation = new AdminNavigation();
+            MainContent = navigation.GetPage("online");
+
+            GoToOnlineUsersCommand = new DelegateCommand(GoToOnlineUsers);
+        }
+
+        private void GoToOnlineUsers(object obj)
+        {
+            MainContent = navigation.GetPage("online");
+        }
+    }
+}
