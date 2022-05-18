e9f141ff46eadd1586d58fc39f332356ef914bb7
Alexander <alexandrmozolevskiy@gmail.com>
2022-05-18 21:38:19 +0300

    Tests started

---
+++ MessengerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MessengerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MessengerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bd79085e-cd91-4977-9e8f-e0e7e205c184")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MessengerTests/UnitTest1.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using WCFService.DTO;
+using WCFService.Unit;
+
+namespace MessengerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Can_Register()
+        {
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                Media media = new Media()
+                {
+                    Path = "Path"
+                };
+                unitOfWork.MediaRepository.Add(media);
+                unitOfWork.Save();
+
+                UserAuth userAuth = new UserAuth()
+                {
+                    Login = "Login",
+                    Password = "Password"
+                };
+                unitOfWork.UserAuthRepository.Add(userAuth);
+                unitOfWork.Save();
+
+                User user = new User()
+                {
+                    Name = "Name",
+                    Surname = "Surname",
+                    Role = "user",
+                    Media = media,
+                    UserAuth = userAuth
+                };
+
+                unitOfWork.UserRepository.Add(user);
+                unitOfWork.Save();
+
+                Assert.AreNotEqual(user.Id, 0);
+            }
+        }
+
+        [TestMethod]
+        public void Can_Update_Avatar()
+        {
+            string path = "Path";
+            string path_updated = "Path_updated";
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                Media media = new Media()
+                {
+                    Path = path
+                };
+                unitOfWork.MediaRepository.Add(media);
+                unitOfWork.Save();
+
+                UserAuth userAuth = new UserAuth()
+                {
+                    Login = "Login",
+                    Password = "Password"
+                };
+                unitOfWork.UserAuthRepository.Add(userAuth);
+                unitOfWork.Save();
+
+                User user = new User()
+                {
+                    Name = "Name",
+                    Surname = "Surname",
+                    Role = "user",
+                    Media = media,
+                    UserAuth = userAuth
+                };
+
+                unitOfWork.UserRepository.Add(user);
+                unitOfWork.Save();
+
+                user.Media.Path = path_updated;
+                unitOfWork.UserRepository.Update(user);
+                unitOfWork.Save();
+
+                Assert.AreEqual(user.Media.Path, path_updated);
+            }
+        }
+
+        [TestMethod]
+        public void Can_Create_Chat()
+        {
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                Media media = new Media()
+                {
+                    Path = "Path"
+                };
+                unitOfWork.MediaRepository.Add(media);
+                unitOfWork.Save();
+
+                UserAuth userAuth = new UserAuth()
+                {
+                    Login = "Login",
+                    Password = "Password"
+                };
+                unitOfWork.UserAuthRepository.Add(userAuth);
+                unitOfWork.Save();
+
+                User user = new User()
+                {
+                    Name = "Name",
+                    Surname = "Surname",
+                    Role = "user",
+                    Media = media,
+                    UserAuth = userAuth
+                };
+
+                unitOfWork.UserRepository.Add(user);
+                unitOfWork.Save();
+
+                Chat chat = new Chat()
+                {
+                    Name = "Name",
+                    Admin = user,
+                    Media = media
+                };
+
+                unitOfWork.ChatRepository.Add(chat);
+                unitOfWork.Save();
+
+                Assert.AreNotEqual(chat.Id, 0);
+            }
+        }
+
+        [TestMethod]
+        public void Can_Accept_Report()
+        {
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                Media media = new Media()
+                {
+                    Path = "Path"
+                };
+                unitOfWork.MediaRepository.Add(media);
+                unitOfWork.Save();
+
+                UserAuth userAuth = new UserAuth()
+                {
+                    Login = "Login",
+                    Password = "Password"
+                };
+                unitOfWork.UserAuthRepository.Add(userAuth);
+                unitOfWork.Save();
+
+                User user = new User()
+                {
+                    Name = "Name",
+                    Surname = "Surname",
+                    Role = "user",
+                    Media = media,
+                    UserAuth = userAuth
+                };
+
+                unitOfWork.UserRepository.Add(user);
+                unitOfWork.Save();
+
+                Chat chat = new Chat()
+                {
+                    Name = "Name",
+                    Admin = user,
+                    Media = media
+                };
+
+                unitOfWork.ChatRepository.Add(chat);
+                unitOfWork.Save();
+
+                Message message = new Message()
+                {
+                    Text = "Text",
+                    User = user,
+                    Chat = chat,
+                    Media = media,
+                    Date = DateTime.Now
+                };
+
+                unitOfWork.MessageRepository.Add(message);
+                unitOfWork.Save();
+
+                Report report = new Report()
+                {
+                    Message = message
+                };
+
+                unitOfWork.ReportRepository.Add(report);
+                unitOfWork.Save();
+
+                Message reported = unitOfWork.MessageRepository.GetAll().Where(m => m.Id == report.Message.Id).FirstOrDefault();
+                reported.Text = "This message was hidden by admin";
+                unitOfWork.MessageRepository.Update(reported);
+                unitOfWork.Save();
+
+                Assert.AreEqual(reported.Text, "This message was hidden by admin");
+            }
+        }
+    }
+}
